47f3eaf166744d716ac75e8bca8743c7d760c2dc
Lepfner <lerner.andi@gmail.com>
2021-07-02 00:27:02 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="text-center bg-primary">
+      <div className="flex flex-col text-4xl justify-center items-center min-h-screen">
+        <div className="w-max rounded-xl border-2 border-solid border-purple-800 bg-purple-800 shadow-xl">
+          <h1 className="text-white p-10 pb-5">Andrija Lerner - Hello World</h1>
+          <p className="text-2xl text-white">Welcome! :D</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'primary': '#282c34',
+       })
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
