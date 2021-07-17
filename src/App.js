8ff2d60cf22410c54794bf1ba6d5e79690362e7d
Lepfner <lerner.andi@gmail.com>
2021-07-17 14:03:59 +0200

    Login page

--- src/App.js
+++ src/App.js
@@ -1,42 +1,70 @@
-import React from "react";
-import SkyLight from 'react-skylight';
+import './App.css';
+import {BrowserRouter as Router, Route, Link, Switch, Redirect} from "react-router-dom";
+import {React, useState} from 'react';
+import MainMenu from './components/Dashboard';
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
+const App = () => {
+
+  const [address, setAddress] = useState('');
+  const [redirect, setRedirect] = useState(false);
+  const [auth, setAuth] = useState();
+
+  const submit = async (e) => {
+    e.preventDefault();
+    await fetch('http://localhost:3000/api/login', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      credentials: 'include',
+      body: JSON.stringify({
+        address
+      })
+    });
+    {/*const response = await fetch('http://localhost:3000/api/userAuth', {
+        headers: {'Content-Type': 'application/json'},
+        credentials: 'include',
+    });
+    const authState = await response.json();
+    setAuth(authState.state);
+    if (auth) {
+      setRedirect (true);
+    } else {
+      alert (`Neispravan unos podataka!`);
+    }
+  */}
+    setRedirect (true);
   }
 
-  render() {
+  if (redirect){
+    return <Router><Route exact path="/dashboard" component={MainMenu}></Route><Redirect to="/dashboard"/></Router>
+  }
 
   return (
-    <div className="text-center bg-primary">
-      <div className="flex flex-col text-4xl justify-center items-center min-h-screen">
-        <div className="w-max rounded-xl border-2 border-solid border-purple-800 bg-purple-800 shadow-xl hover:border-yellow-500 hover:bg-yellow-500 hover:animate-pulse duration-1000">
-          <h1 className="text-white p-10 mb-5 pb-5">Andrija Lerner - Hello World</h1>
-          <p className="text-2xl mb-4 text-white">Welcome! :D</p>
-        </div>
-        <div className="w-max h-max rounded-xl border-2 border-solid border-purple-800 bg-purple-800 shadow-xl m-6 hover:border-yellow-500 hover:bg-yellow-500 hover:animate-pulse duration-1000">
-          <button className="text-white text-xl m-6" onClick={() => this.simpleDialog.show()}>About me</button>
-        </div>
-        <SkyLight hideOnOverlayClicked ref={ref => this.simpleDialog = ref} title="Andrija Lerner">
-          <p className="text-lg">19 years old software developer ready to learn and expand his knowledge and opportunities in computer science.</p>
-          <div className="text-left text-lg">
-            <p>Skills:</p>
-            <p>- Frontend development (React.js, Next.js, HTML, CSS, JS, ASP.NET (C#), Tailwind.css)</p>
-            <p>- Backend development (MySQL)</p>
-            <p>- Video game development (C++, Unreal Engine)</p>
-            <p>- Video editing (Sony Vegas 14, Adobe Premiere pro)</p>
-            <p>Education:</p>
-            <p>- Elektrotehnička škola Split - Tehničar za računalstvo (2016-2020)</p>
-            <p>- Fakultet elektrotehnike, strojarstva i brodogradnje (2020-?)</p>
-            <p>Mail: lerner.andi@gmail.com , alerne00@fesb.hr</p>
-            <a className="text-yellow-500" href="https://www.instagram.com/lepfner_/" target="_blank" rel="noopener noreferrer">My Instagram</a>
+    <div>
+          <div className="test">
+            <div className="fixed top-2/4 left-2/4 bg-white z-50 transform -translate-x-1/2 -translate-y-1/2 w-900 h-500 rounded-6x1 shadow-lg">
+              <div className="float-left block w-leftCol h-full">
+                <h1 className="text-11x1 font-bold mt-7 pl-login mb-12 text-black">LOGIN</h1>
+                <form onSubmit={submit}>
+                <div className="border-r border-solid border-primary flex flex-col justify-center items-center">
+                  <label className="italic text-black text-7x1 self-start ml-18">Welcome aboard!</label><br/>
+                  <input 
+                  placeholder="Ethererum wallet address" 
+                  required 
+                  type="text" 
+                  className=" pl-2 h-11 text-7x1 w-88 border-solid border border-primary rounded-4x1 bg-fourth outline-none text-black"
+                  onChange={e => {setAddress(e.target.value)}}
+                  /><br/>
+                  <button type="submit" className="bg-primary border-solid border focus:outline-none rounded-5x1 w-88 h-12 text-white text-7x1">Login</button>
+                </div>
+              </form>
+            </div>
+            <div className="float-right block w-rightCol h-full flex flex-col justify-center items-center">
+              <img className="h-2/3 w-3/4 animate-pulse duration-1000" src="Ethereum.png" alt=''/>
+            </div>
           </div>
-        </SkyLight>
-      </div>
-    </div>
+        </div>
+    </div>  
   );
-}
 }
 
 export default App;
