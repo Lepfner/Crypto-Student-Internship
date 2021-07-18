de7d1ba1c442db8700459e55fdba3a81a7fe1f72
Lepfner <lerner.andi@gmail.com>
2021-07-18 19:40:46 +0200

    Dashboard design

--- src/App.js
+++ src/App.js
@@ -39,31 +39,31 @@ const App = () => {
   }
 
   return (
-    <div>
           <div className="test">
             <div className="fixed top-2/4 left-2/4 bg-white z-50 transform -translate-x-1/2 -translate-y-1/2 w-900 h-500 rounded-6x1 shadow-lg">
+              {/*Livi dio*/}
               <div className="float-left block w-leftCol h-full">
                 <h1 className="text-11x1 font-bold mt-7 pl-login mb-12 text-black">LOGIN</h1>
                 <form onSubmit={submit}>
-                <div className="border-r border-solid border-primary flex flex-col justify-center items-center">
-                  <label className="italic text-black text-7x1 self-start ml-18">Welcome aboard!</label><br/>
-                  <input 
-                  placeholder="Ethererum wallet address" 
-                  required 
-                  type="text" 
-                  className=" pl-2 h-11 text-7x1 w-88 border-solid border border-primary rounded-4x1 bg-fourth outline-none text-black"
-                  onChange={e => {setAddress(e.target.value)}}
-                  /><br/>
-                  <button type="submit" className="bg-primary border-solid border focus:outline-none rounded-5x1 w-88 h-12 text-white text-7x1">Login</button>
-                </div>
-              </form>
-            </div>
+                  <div className="border-r border-solid border-primary flex flex-col justify-center items-center">
+                    <label className="italic text-black text-7x1 self-start ml-18">Welcome aboard!</label><br/>
+                    <input 
+                    placeholder="Ethererum wallet address" 
+                    required 
+                    type="password" 
+                    className=" pl-2 h-11 text-7x1 w-88 border-solid border border-primary rounded-4x1 bg-fourth outline-none text-black"
+                    onChange={e => {setAddress(e.target.value)}}/>
+                    <br/>
+                    <button type="submit" className="bg-primary border-solid border focus:outline-none rounded-5x1 w-88 h-12 text-white text-7x1">Login</button>
+                  </div>
+                </form>
+              </div>
+            {/*Desni dio*/}
             <div className="float-right block w-rightCol h-full flex flex-col justify-center items-center">
               <img className="h-2/3 w-3/4 animate-pulse duration-1000" src="Ethereum.png" alt=''/>
             </div>
           </div>
-        </div>
-    </div>  
+        </div> 
   );
 }
 
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faWallet, faExchangeAlt, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 function Dashboard() {
     return (
-        <div>
-            
+        <div className="test">
+            <div className="fixed top-2/4 left-2/4 bg-white transform -translate-x-1/2 -translate-y-1/2 w-2/3 h-2/3 rounded-6x1 shadow-lg flex flex-col justify-around items-center">
+                <div className="h-1/3 w-full flex justify-center items-center">
+                    <h1 className="text-10x1 italic text-black">Lepfner's blockchain explorer</h1>
+                </div>
+                <div className="h-1/3 w-full flex justify-around items-center">
+                    <div className="w-3/12 h-full">
+                        <button className="text-8x1 italic bg-purple-700 border-solid border focus:outline-none rounded-5x1 w-full h-full">Explore<br/>
+                        <FontAwesomeIcon icon={faSearch} color="white"/>
+                        </button>
+                    </div>
+                    <div className="w-3/12 h-full">
+                        <button className="text-8x1 italic bg-purple-700 border-solid border focus:outline-none rounded-5x1 w-full h-full">Mint<br/>
+                        <FontAwesomeIcon icon={faWallet} color="white"/>
+                        </button>
+                    </div>
+                    <div className="w-3/12 h-full">
+                        <button className="text-8x1 italic bg-purple-700 border-solid border focus:outline-none rounded-5x1 w-full h-full">Transfer<br/>
+                        <FontAwesomeIcon icon={faExchangeAlt} color="white"/>
+                        </button>
+                    </div>
+                </div>
+                <div className="h-1/3 w-full">
+                    <button className="absolute top-0 right-0 bg-purple-700 border-solid border focus:outline-none rounded-5x1 w-1/12 h-1/12 mt-2 mr-4">Logout</button>
+                </div>
+            </div>
         </div>
     )
 }
