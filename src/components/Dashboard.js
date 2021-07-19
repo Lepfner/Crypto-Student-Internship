f7ba4d7450a122820ec72302881fdc74e3c01350
Lepfner <lerner.andi@gmail.com>
2021-07-19 19:35:33 +0200

    Added router

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,30 +1,37 @@
 import React from 'react';
+import Explore from './Explore'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faWallet, faExchangeAlt, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import {BrowserRouter as Router, Route, Link, Switch, Redirect} from "react-router-dom";
 
 function Dashboard() {
     return (
-        <div className="test">
+        <Router>
+            <Switch>
+            <Route exact path="/explore" component={Explore}/>
+            <div className="test">
             <div className="fixed top-2/4 left-2/4 bg-white transform -translate-x-1/2 -translate-y-1/2 w-2/3 h-2/3 rounded-6x1 shadow-lg flex flex-col justify-around items-center">
                 <div className="h-1/3 w-full flex justify-center items-center">
                     <h1 className="text-10x1 italic text-black">Lepfner's blockchain explorer</h1>
                 </div>
                 <div className="h-1/3 w-full flex justify-around items-center">
                     <div className="w-3/12 h-full">
+                        <Link to="/explore">
                         <button className="wrapper relative text-8x1 italic bg-purple-700 border-solid border focus:outline-none rounded-5x1 w-full h-full hover:text-purple-700 hover:opacity-50 duration-300">Explore<br/>
-                        <p className="content text-white absolute top-2/4 left-2/4 transform -translate-x-1/2 -translate-y-1/2">Explore this div</p>
+                        <p className="content text-white absolute top-2/4 left-2/4 transform -translate-x-1/2 -translate-y-1/2">Explore other addresses and discover their Ether amount</p>
                         <FontAwesomeIcon icon={faSearch} color="white" className="h-7 wrapper-image"/>
                         </button>
+                        </Link>
                     </div>
                     <div className="w-3/12 h-full">
                         <button className="wrapper relative text-8x1 italic bg-purple-700 border-solid border focus:outline-none rounded-5x1 w-full h-full hover:text-purple-700 hover:opacity-50 duration-300">Mint<br/>
-                        <p className="content text-white absolute top-2/4 left-2/4 transform -translate-x-1/2 -translate-y-1/2">Explore this div</p>
+                        <p className="content text-white absolute top-2/4 left-2/4 transform -translate-x-1/2 -translate-y-1/2">Add Ether to your address</p>
                         <FontAwesomeIcon icon={faWallet} color="white" className="h-7 wrapper-image"/>
                         </button>
                     </div>
                     <div className="w-3/12 h-full">
                         <button className="wrapper relative text-8x1 italic bg-purple-700 border-solid border focus:outline-none rounded-5x1 w-full h-full hover:text-purple-700 hover:opacity-50 duration-300">Transfer<br/>
-                        <p className="content text-white absolute top-2/4 left-2/4 transform -translate-x-1/2 -translate-y-1/2">Explore this div</p>
+                        <p className="content text-white absolute top-2/4 left-2/4 transform -translate-x-1/2 -translate-y-1/2">Transfer Ether to another address</p>
                         <FontAwesomeIcon icon={faExchangeAlt} color="white" className="h-7 wrapper-image"/>
                         </button>
                     </div>
@@ -33,7 +40,10 @@ function Dashboard() {
                     <button className="absolute top-0 right-0 bg-purple-700 border-solid border focus:outline-none rounded-5x1 w-1/12 h-1/12 mt-2 mr-4">Logout</button>
                 </div>
             </div>
-        </div>
+            </div>
+            </Switch>
+        </Router>
+        
     )
 }
 
