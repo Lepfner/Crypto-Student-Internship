b49b6afc1da641fbd8d96b43a1da83be8261ae27
Lepfner <lerner.andi@gmail.com>
2021-08-02 17:52:30 +0200

    ERC20 Standard

---
+++ contracts/contracts/TestContract.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestContract is ERC20{
+    constructor() ERC20("MinerRewards", "MRW"){}
+}
