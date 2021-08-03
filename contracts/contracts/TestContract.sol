ff185caa5a1c80f07041d75629647b487f4acb33
Lepfner <lerner.andi@gmail.com>
2021-08-03 15:49:37 +0200

    Deployed contract

--- contracts/contracts/TestContract.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
-
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract TestContract is ERC20{
-    constructor() ERC20("MinerRewards","MRW"){}
-}
--- contracts/migrations/2_deploy_contracts.js
+++ contracts/migrations/2_deploy_contracts.js
@@ -1,10 +1,6 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
-var HelloWorld = artifacts.require ("./TestContract.sol");
+var myToken = artifacts.require("./myToken.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
-  deployer.deploy(HelloWorld);
-};
+module.exports = function (deployer) {
+    deployer.deploy(myToken);
+  };
+  
