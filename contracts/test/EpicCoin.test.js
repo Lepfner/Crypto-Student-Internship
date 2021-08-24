3ce82bf1f08b2f8861850f462899ee990002bf3c
Lepfner <lerner.andi@gmail.com>
2021-08-24 12:49:45 +0200

    Unit tests

--- contracts/test/EpicCoin.test.js
+++ contracts/test/EpicCoin.test.js
@@ -1,10 +1,58 @@
-const EpicCoin = artifacts.require("EpicCoin")
+const EpicCoin = artifacts.require("EpicCoin");
+const { expect } = require('chai');
+const { BN, expectEvent, expectRevert, constants } = require('@openzeppelin/test-helpers');
+
+//name
+//symbol
+//totalSupply
+//balanceOf
+//transfer
+
+//mint
+//allowance
+//approve
+//transferFrom
+//increaseAllowance
+//decreaseAllowance
 
 contract("EpicCoin", (accounts) =>{
 
+    const NAME = 'EpicCoin';
+    const SYMBOL = 'EC';
+    const FAKENAME = 'FakeCoin';
+    const DECIMALS = 18;
+    const TOTAL_SUPPLY = new BN('10000000000000000000000');
+
     before(async () =>{
-        epiccoin = await EpicCoin.deployed()
+        epiccoin = await EpicCoin.deployed();
     })
+
+      it('retrieve returns a value previously stored', async function () {
+        expect(await epiccoin.totalSupply()).to.be.bignumber.equal(TOTAL_SUPPLY);
+      });
+    
+      it('has a name', async function () {
+        expect(await epiccoin.name()).to.be.equal(NAME);
+      });
+
+      it('has fake name', async function(){
+        expect(await epiccoin.name()).to.be.equal(FAKENAME);
+      });
+    
+      it('has a symbol', async function () {
+        expect(await epiccoin.symbol()).to.be.equal(SYMBOL);
+      });
+
+      it('can mint tokens', async function (){
+        let balance2 = await epiccoin.balanceOf(accounts[0]);
+        balance2 = web3.utils.fromWei(balance2,'ether')
+        assert.equal(balance2, '10000', "Balance should be 10k tokens for contract creator")
+      });
+
+      it('assigns the initial total supply to the creator', async function () {
+        expect(await epiccoin.balanceOf(accounts[0])).to.be.bignumber.equal(TOTAL_SUPPLY);
+      });
+
 
     it("can transfer tokens between accounts", async ()=>{
         let amount = web3.utils.toWei('1000','ether')
